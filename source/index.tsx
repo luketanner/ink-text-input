af49585b8133f50c74dc4f87227567267d1427f0
Luke Tanner <luke.c.tanner@gmail.com>
2024-06-09 19:06:44 +1000

    Implement additional bash/zsh/emacs motions

--- source/index.tsx
+++ source/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Text, useInput } from 'ink';
 import chalk from 'chalk';
 import type { Except } from 'type-fest';
@@ -113,6 +113,33 @@ function TextInput({
 	const start = 0;
 	const end = originalValue.length;
 
+	const wordBoundaries = useMemo(() => {
+		// "in-betweens" in the sense of not including the boundary between the first character and the start of the
+		// string, nor the boundary between the final character and the end of the string.
+		const inBetweens = [...originalValue.matchAll(/[\W_][^\W_]/g)].map(match => match.index + 1);
+
+		// The only time this should result in duplicate entries is if `originalValue` is an empty string: the result
+		// will be `[0, 0]` -- but in that scenario there are no other boundaries to navigate to anyway, so the
+		// duplicate entries shouldn't have any practical consequence, so no point trying to handle it.
+		return [start, ...inBetweens, end];
+	}, [originalValue]);
+
+	const [prevBoundary, nextBoundary] = useMemo(() => {
+		let prev = wordBoundaries[0]!;
+		let next = wordBoundaries[wordBoundaries.length - 1]!;
+
+		for (const boundary of wordBoundaries) {
+			if (boundary < cursorOffset) {
+				prev = boundary;
+			} else if (boundary > cursorOffset) {
+				next = boundary;
+				break;
+			}
+		}
+
+		return [prev, next];
+	}, [wordBoundaries, cursorOffset]);
+
 	useInput(
 		(input, key) => {
 			if (key.upArrow || key.downArrow || (key.ctrl && input === 'c') || key.tab || (key.shift && key.tab)) {
@@ -135,27 +162,68 @@ function TextInput({
 			let nextValue = originalValue;
 			let nextCursorWidth = 0;
 
-			if (key.leftArrow) {
+			if (key.ctrl && input === 'a') {
+				if (showCursor) {
+					nextCursorOffset = start;
+				}
+			} else if (key.ctrl && input === 'e') {
+				if (showCursor) {
+					nextCursorOffset = end;
+				}
+			} else if ((key.ctrl && key.leftArrow) || (key.meta && input === 'b')) {
+				if (showCursor) {
+					nextCursorOffset = prevBoundary;
+				}
+			} else if ((key.ctrl && key.rightArrow) || (key.meta && input === 'f')) {
+				if (showCursor) {
+					nextCursorOffset = nextBoundary;
+				}
+			} else if (key.leftArrow || (key.ctrl && input === 'b')) {
 				if (showCursor && cursorOffset > start) {
 					nextCursorOffset--;
 				}
-			} else if (key.rightArrow) {
+			} else if (key.rightArrow || (key.ctrl && input === 'f')) {
 				if (showCursor && cursorOffset < end) {
 					nextCursorOffset++;
 				}
-			} else if (key.backspace || key.delete) {
+			} else if (key.ctrl && input === 'u') {
 				if (cursorOffset > start) {
-					nextValue =
-						originalValue.slice(start, cursorOffset - 1) +
-						originalValue.slice(cursorOffset, end);
-
+					nextValue = originalValue.slice(cursorOffset, end);
+					nextCursorOffset = start;
+				}
+			} else if (key.ctrl && input === 'k') {
+				if (cursorOffset < end) {
+					nextValue = originalValue.slice(start, cursorOffset);
+					// `nextCursorOffset` remains unchanged
+				}
+			} else if (key.ctrl && input === 'w') {
+				if (cursorOffset > start) {
+					nextValue = originalValue.slice(start, prevBoundary) + originalValue.slice(cursorOffset, end);
+					nextCursorOffset = prevBoundary;
+				}
+			} else if (key.meta && input === 'd') {
+				// NOTE: This is different to the `alt+d` behavior in both bash and zsh -- but in my defence, zsh is
+				// weirdly inconsistent here, in that it deletes to the _end_ of the _current_ word (which is how bash
+				// does forwards deletion and _navigation_), rather than  to the _start_ of the _next_ word (which is
+				// how zsh does forwards navigation -- but apparently not deletion? why??).
+				if (cursorOffset < end) {
+					nextValue = originalValue.slice(start, cursorOffset) + originalValue.slice(nextBoundary, end);
+					// `nextCursorOffset` remains unchanged
+				}
+			} else if (key.backspace || (key.ctrl && input === 'h')) {
+				// Note: Checking for `ctrl+h` for completeness and clarity, but possibly unnecessary? Since some (if
+				// not all?) terminal emulators intercept `ctrl+h` and send a backspace sequence directly.
+				if (cursorOffset > start) {
+					nextValue = originalValue.slice(start, cursorOffset - 1) + originalValue.slice(cursorOffset, end);
 					nextCursorOffset--;
 				}
+			} else if (key.delete || (key.ctrl && input === 'd')) {
+				if (cursorOffset < end) {
+					nextValue = originalValue.slice(start, cursorOffset) + originalValue.slice(cursorOffset + 1, end);
+					// `nextCursorOffset` remains unchanged
+				}
 			} else {
-				nextValue =
-					originalValue.slice(start, cursorOffset) +
-					input +
-					originalValue.slice(cursorOffset, end);
+				nextValue = originalValue.slice(start, cursorOffset) + input + originalValue.slice(cursorOffset, end);
 
 				nextCursorOffset += input.length;
 
